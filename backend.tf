9c1450eb7d303e886c0583512b57bb2f0f52cc7e
jsanwo <jsanwo@gmail.com>
2022-10-28 18:25:46 -0400

    bucket name updated

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket"
+    bucket = "tfs3jenkinsterraform"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
